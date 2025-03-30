8b1824a41387dd6ea534770441252b9bc5ac9a47
Nhutnguyen395 <nhutnguyen395@gmail.com>
2025-03-30 13:20:10 -0700

    edit helpers.h

--- include/helpers.h
+++ include/helpers.h
@@ -1,8 +1,15 @@
-#ifndef HELPERS.H
-#define HELPERS.H
+#ifndef HELPERS_H
+#define HELPERS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "shell.h"
+
+node_t* createNode(int id, int units);
+void addToList(node_t** head, int id, int units);
+int removeFromList(node_t** head, int id, int* units);
+int isInList(node_t* head, int id, int* units);
+void addToRL(int process_id, int priority);
+int removeFromRL(int process_id);
+int getHighestPriorityProcess();
+void freeList(node_t** head);
 
 #endif 
