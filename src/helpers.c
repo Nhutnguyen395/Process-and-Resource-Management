5169568e33ad9f44ac8cfe0d0e0d4d8d9ae3707e
Nhutnguyen395 <nhutnguyen395@gmail.com>
2025-03-30 13:56:56 -0700

    edit helpers.c

---
+++ src/helpers.c
@@ -0,0 +1,31 @@
+#include "../include/helpers.h"
+
+extern process_control_block pcb[16];
+extern resource_control_block rcb[4];
+extern ready_list rl;
+
+node_t* createNode(int id, int units){
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    if (newNode == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->id = id;
+    newNode->units = units;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addToList(node_t** head, int id, int units){
+    node_t* newNode = createNode(id, units);
+    if (*head == NULL){
+        *head = newNode;
+        return;
+    } 
+    
+    node_t* current = *head;
+    while (current->next != NULL){
+        current = current->next;
+    }
+    current->next = newNode;
+}
--- src/shell.c
+++ src/shell.c
@@ -1,3 +1,6 @@
-#include "shell.h"
-#include "helpers.h"
+#include "../include/shell.h"
+#include "../include/helpers.h"
 
+process_control_block pcb[16];
+resource_control_block rcb[4];
+ready_list rl;
